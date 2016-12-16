3df3053279512dcdb6be0939868fbbcd6892caf5
Zahir Iqbal <zahiriqbal@users.noreply.github.com>
2016-12-16 17:58:04 +0000

    Add files via upload

---
+++ RPS/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace RPS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.player1 = new System.Windows.Forms.CheckedListBox();
+            this.player2 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(101, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(42, 40);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(59, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Comp1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(171, 40);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(59, 17);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Comp2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // player1
+            // 
+            this.player1.FormattingEnabled = true;
+            this.player1.Items.AddRange(new object[] {
+            "Rock",
+            "Paper",
+            "Scissor"});
+            this.player1.Location = new System.Drawing.Point(42, 78);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(68, 49);
+            this.player1.TabIndex = 3;
+            // 
+            // player2
+            // 
+            this.player2.FormattingEnabled = true;
+            this.player2.Items.AddRange(new object[] {
+            "Rock",
+            "Paper",
+            "Scissor"});
+            this.player2.Location = new System.Drawing.Point(171, 78);
+            this.player2.Name = "player2";
+            this.player2.Size = new System.Drawing.Size(68, 49);
+            this.player2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 205);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(376, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "If you want one or both player to a computer player tick Comp1 and/or Comp2.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 262);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.player2);
+            this.Controls.Add(this.player1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckedListBox player1;
+        private System.Windows.Forms.CheckedListBox player2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ RPS/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace RPS
+{
+    public partial class Form1 : Form
+    {
+        private List<string> RPS = new List<string>();
+        private Random randNum = new Random();
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            RPS.Clear();
+            RPS.Add("Rock");
+            RPS.Add("Paper");
+            RPS.Add("Scissor");
+        }
+
+
+        public string calcWins()
+        {
+
+            string message = string.Empty;
+
+            if(player1.SelectedItem == null ) { return message = "Player 1 select your option to play!";}
+            if (player2.SelectedItem == null) { return message = "Player 2 select your option to play!"; }
+
+            //Draws
+            if (player1.SelectedItem.ToString() == "Rock" && player2.SelectedItem.ToString() == "Rock") { message = "Its a draw!"; }
+            if (player1.SelectedItem.ToString() == "Paper" && player2.SelectedItem.ToString() == "Paper") { message = "Its a draw!"; }
+            if (player1.SelectedItem.ToString() == "Scissor" && player2.SelectedItem.ToString() == "Scissor") { message = "Its a draw!"; }
+
+            //Player 1
+            if (player1.SelectedItem.ToString() == "Rock" && player2.SelectedItem.ToString() == "Scissor") { message = "Player 1 wins!"; }
+            if (player1.SelectedItem.ToString() == "Rock" && player2.SelectedItem.ToString() == "Paper") { message = "Player 2 wins!"; }
+
+            if (player1.SelectedItem.ToString() == "Paper" && player2.SelectedItem.ToString() == "Scissor") { message = "Player 2 wins!"; }
+            if (player1.SelectedItem.ToString() == "Paper" && player2.SelectedItem.ToString() == "Rock") { message = "Player 1 wins!"; }
+
+            if (player1.SelectedItem.ToString() == "Scissor" && player2.SelectedItem.ToString() == "Rock") { message = "Player 2 wins!"; }
+            if (player1.SelectedItem.ToString() == "Scissor" && player2.SelectedItem.ToString() == "Paper") { message = "Player 1 wins!"; }
+
+            //Player 2
+            if (player2.SelectedItem.ToString() == "Rock" && player1.SelectedItem.ToString() == "Scissor") { message = "Player 2 wins!"; }
+            if (player2.SelectedItem.ToString() == "Rock" && player1.SelectedItem.ToString() == "Paper") { message = "Player 1 wins!"; }
+
+            if (player2.SelectedItem.ToString() == "Paper" && player1.SelectedItem.ToString() == "Scissor") { message = "Player 1 wins!"; }
+            if (player2.SelectedItem.ToString() == "Paper" && player1.SelectedItem.ToString() == "Rock") { message = "Player 2 wins!"; }
+
+            if (player2.SelectedItem.ToString() == "Scissor" && player1.SelectedItem.ToString() == "Rock") { message = "Player 1 wins!"; }
+            if (player2.SelectedItem.ToString() == "Scissor" && player1.SelectedItem.ToString() == "Paper") { message = "Player 2 wins!"; }
+
+           return message;
+        }
+
+
+        public string compsPlay1()
+        {
+            int aRandomPos = randNum.Next(RPS.Count);
+            string compChoice1 = RPS[aRandomPos];
+
+
+            player1.SetItemCheckState(aRandomPos, CheckState.Checked);
+            Thread.Sleep(1000);
+            player1.SelectedItem = compChoice1;
+
+            return compChoice1;
+        }
+
+
+        public string compsPlay2()
+        {
+            int aRandomPos = randNum.Next(RPS.Count);
+            string compChoice2 = RPS[aRandomPos];
+
+            player2.SetItemCheckState(aRandomPos, CheckState.Checked);
+            Thread.Sleep(1000);
+            player2.SelectedItem = compChoice2;
+
+            return compChoice2;
+        }
+
+
+
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            bool playa1 = false;
+            bool playa2 = false;
+
+            if (checkBox1.Checked == true) { playa1 = true; }
+            if (checkBox2.Checked == true) { playa2 = true; }
+
+
+            if (playa1 == true && playa2 == true) {
+                compsPlay1();
+                compsPlay2();
+                MessageBox.Show(calcWins());
+            }
+            else if (playa1 == true && playa2 == false) {
+                compsPlay1();
+                MessageBox.Show(calcWins());
+            }
+            else if (playa1 == false && playa2 == true) {
+                compsPlay2();
+                MessageBox.Show(calcWins());
+            }
+            else { 
+                    MessageBox.Show(calcWins());
+                 }
+
+            Form1 fr = new Form1();
+            fr.Show();
+            this.Hide();
+            
+        }
+    }
+}
